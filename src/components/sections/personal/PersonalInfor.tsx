7140273096d014082222b3df16e95b56320d0778
BinhLe <binhle@vietnamblockchain.asia>
2025-04-26 13:55:47 +0700

    chore: update content

--- src/components/sections/personal/PersonalInfor.tsx
+++ src/components/sections/personal/PersonalInfor.tsx
@@ -19,7 +19,7 @@ export default function PersonalInfo() {
         <div className="flex-1 h-px bg-primary/20 mx-2" />
       </div>
       <p className="text-sm md:text-base text-zinc-700 dark:text-zinc-200 mb-8">
-        Front-end Developer with nearly 4 years of experience, specializing in{" "}
+        Front-end Developer with nearly 3 years of experience, specializing in{" "}
         <span className="font-bold text-blue-500">ReactJS</span> and other
         front-end technologies. Skilled in{" "}
         <span className="font-bold text-blue-400">TypeScript</span>,{" "}
@@ -27,16 +27,27 @@ export default function PersonalInfo() {
         <span className="font-bold text-blue-400">Bootstrap</span>,{" "}
         <span className="font-bold text-blue-400">jQuery</span>,{" "}
         <span className="font-bold text-blue-400">Tailwinds</span> and{" "}
-        <span className="font-bold text-blue-400">JavaScript</span>. After 2
-        years at <span className="underline text-blue-400">Halozend</span>, I
-        successfully contributed to projects for clients in the US and Vietnam.
-        Currently, I am leveraging my skills at{" "}
-        <span className="underline text-blue-400">Dision</span> to develop
-        innovative solutions for clients in Germany. Additionally, I have
-        experience integrating APIs based on backend technologies like Golang
-        and PHP, utilizing both NoSQL and SQL databases, and implementing
-        real-time features using{" "}
-        <span className="font-bold text-blue-500">WebSocket</span>. Renowned for
+        <span className="font-bold text-blue-400">JavaScript</span>. After years
+        working at{" "}
+        <a
+          href="https://dision.tech/"
+          target="_blank"
+          className="underline text-blue-400"
+        >
+          DISION Tech LLC
+        </a>
+        , I successfully contributed to projects for clients in the europe and
+        Vietnam. Currently, I am leveraging my skills at{" "}
+        <a
+          href="https://vietnamblockchain.asia/"
+          target="_blank"
+          className="underline text-blue-400"
+        >
+          Vietnam Blockchain Corporation (VBC)
+        </a>{" "}
+        to develop smart water management application for clients in the
+        Southern of Vietnam. Additionally, I have experience integrating APIs
+        based on backend technologies like Restful and GraphQL, . Renowned for
         my commitment to delivering high-quality results and effectively
         tackling complex challenges.
       </p>
@@ -51,13 +62,18 @@ export default function PersonalInfo() {
       <ul className="list-disc list-inside text-sm md:text-base text-zinc-700 dark:text-zinc-200 mb-8 text-left space-y-2">
         <li>
           <span className="font-bold">
-            Posts and Telecommunications Institute of Technology:
+            Ho Chi Minh City University of Technology (Bach Khoa University)
           </span>{" "}
-          Engineering degree in technology (2021)
+          2017 - 2021
         </li>
         <li>
-          <span className="font-bold">Knowledge:</span> C++, C#, Java, Microsoft
-          SQL Server,...
+          <span className="font-bold">Tra Vinh University</span> 2024 - now
+        </li>
+        <li>
+          <span className="font-bold">Knowledge:</span> React, ReactJs, NextJs, WebSocket,
+          Accessibility, TypeScript, JavaScript, Redux, Ant Design, ShadcnUI,
+          Bootstrap, JavaScript, Next.js, REST API, Git, Figma,
+          Responsive Design...
         </li>
       </ul>
       {/* Technical skills */}
@@ -71,35 +87,25 @@ export default function PersonalInfo() {
       <ul className="list-disc list-inside text-sm md:text-base text-zinc-700 dark:text-zinc-200 space-y-2 text-left">
         <li>
           <span className="font-bold">Programming Languages:</span> Typescript,
-          Javascript, PHP
+          Javascript
         </li>
         <li>
           <span className="font-bold">Frameworks/Platforms:</span> ReactJS,
-          Next.js, Antd, Tailwindcss, Vanilla JS, Wordpress, VueJS, Vitest,
-          Laravel, React Native, Angular, NodeJS, Docker, Bootstrap, Tanstack,
-          ...
+          Next.js, Antd, Tailwindcss, Vanilla JS, Vite, ...
         </li>
         <li>
-          <span className="font-bold">Database Management Systems:</span> MySQL,
-          PostgreSQL, Microsoft SQL Server
-        </li>
-        <li>
-          <span className="font-bold">IDEs:</span> Visual Studio Code, PHPStorm,
-          XCode, Android Studio, Sublime text, Visual Studio
+          <span className="font-bold">IDEs:</span> Visual Studio Code, Windsurf, Visual Studio
         </li>
         <li>
           <span className="font-bold">Operating Systems:</span> Window, Mac OS
         </li>
         <li>
-          <span className="font-bold">Manage Source Code:</span> Gitea, Github,
+          <span className="font-bold">Manage Source Code:</span> Github,
           Gitlab
         </li>
         <li>
           <span className="font-bold">Data Fetching Libraries:</span> Axios,
-          Graphql
-        </li>
-        <li>
-          <span className="font-bold">Web Servers:</span> Apache, Nginx
+          Graphql, Apollo Client 
         </li>
       </ul>
     </motion.section>
